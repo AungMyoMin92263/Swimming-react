a63907dd74d90c1e855335eb1b1149347ce20389
tinyuzana <yuzanamaung2020@gmail.com>
2022-08-16 18:46:12 +0630

    add layout

---
+++ src/interfaces/School.ts
@@ -0,0 +1,9 @@
+export interface School {
+    id: number,
+    created_by: number,
+    created_at: Date,
+    updated_at: Date,
+    deleted_at: Date,
+    name: string,
+    logo: string,
+  }
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
-import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
-import MenuBarTag from '../Layouts/menuBar';
-import SideBar from '../Layouts/menuSideBar';
+import React from "react";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import MenuBarTag from "../Layouts/menuBar";
+import SideBar from "../Layouts/menuSideBar";
+
+//import pages
+import AdminDashboardPage from "../pages/admin/AdminDashboard";
+import AdminLoginPage from "../pages/admin/AdminLogin";
+import AdminWelcomePage from "../pages/admin/AdminWelcome";
 
 const routes = [
   {
@@ -9,53 +14,88 @@ const routes = [
     child: [
       {
         path: "login",
-        sideBar: <SideBar></SideBar>,
-        main: <h1>Hello World Main</h1>,
-        footer: <MenuBarTag></MenuBarTag>
+        sideBar: <></>,
+        main: <AdminLoginPage />,
+        footer: <></>,
+        isSideBar: false,
       },
       {
-        path: "signup",
+        path: "welcome",
+        sideBar: <></>,
+        main: <AdminLoginPage />,
+        footer: <></>,
+        isSideBar: false,
+      },
+      {
+        path: "dashboard",
         sideBar: <SideBar></SideBar>,
-        main: <h1>Hello World Sign Up</h1>,
-        footer: <MenuBarTag></MenuBarTag>
-      }
-    ]
+        main: <AdminDashboardPage />,
+        footer: <></>,
+        isSideBar: true,
+      },
+    ],
   },
-
-]
+];
 
 class RouteList extends React.Component {
+  renderPage = (child: any, index: any) => {
+    if (child.isSideBar) {
+      return (
+        <Route
+          path={child.path}
+          key={index + "child-aside"}
+          element={
+            <div className="main-layout">
+              <div>
+                <div className="shadow-box"></div>
+                {child.sideBar}
+              </div>
+              <div className="main-body">{child.main}</div>
+              <div className="footer-menu">{child.footer}</div>
+            </div>
+          }
+        />
+      );
+    } else {
+      return (
+        <Route
+          path={child.path}
+          key={index + "child-aside"}
+          element={
+            <div className="main-layout">
+              <div>
+                <div className="banner-box"></div>
+                <img src="../../../assets/images/banner.png" alt="banner" className="banner-image" />
+              </div>
+              <div className="main-body">{child.main}</div>
+              <div className="footer-menu">{child.footer}</div>
+            </div>
+          }
+        />
+      );
+    }
+  };
 
   render() {
-    return (<div>
-      <BrowserRouter>
-        <Routes>
-          {routes.map((route, index) => (
-            <Route
-              key={index + 'app-aside'}
-              path={route.path}
-              element={
-                <div className='app-layout'>
-                  <Outlet />
-                </div>
-              } >
-              {route.child.map((child, index) => (
-                // <div>
-                <Route path={child.path} key={index + 'child-aside'}
-                  element={
-                    <div className='main-layout'>
-                      <div>
-                        <div className='shadow-box'></div>
-                        {child.sideBar}
-                      </div>
-                      <div className='main-body'>{child.main}</div>
-                      <div className='footer-menu'>{child.footer}</div>
-                    </div>
-                  }
-                />
-              ))}
-              {/* <Route index element={<Home />} /> */}
-              {/* {route.child.map((child, index) => (
+    return (
+      <div>
+        <BrowserRouter>
+          <Routes>
+            {routes.map((route, index) => (
+              <Route
+                key={index + "app-aside"}
+                path={route.path}
+                element={
+                  <div className="app-layout">
+                    <Outlet />
+                  </div>
+                }
+              >
+                {route.child.map((child, index) => (
+                  <>{this.renderPage(child, index)}</>
+                ))}
+                {/* <Route index element={<Home />} /> */}
+                {/* {route.child.map((child, index) => (
                 // <div>
                 <Route path={child.path} key={index + 'child-aside'}
                   element={
@@ -79,13 +119,12 @@ class RouteList extends React.Component {
                   }
                 />
               ))} */}
-
-            </Route>))
-          }
-        </Routes>
-      </BrowserRouter>
-    </div>)
+              </Route>
+            ))}
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
   }
-
 }
 export default RouteList;
